ea2a5c37c3e00cb34496b85b19715a28cd37a741
= <=>
2024-09-11 15:40:17 +0530

    Refactor routing and module imports for authentication and layouts components

---
+++ src/app/layouts/layouts.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouterModule, RouterLink],
+})
+export class LayoutsModule {}
